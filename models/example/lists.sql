18d5a48e92979617aa553a6d807eff26766180c7
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-03-31 11:50:38 +0530

    Update lists.sql

--- models/example/lists.sql
+++ models/example/lists.sql
@@ -2,71 +2,61 @@
     materialized="table"
 )}}
       
-      select * from(
+      select * from (
 
-with __dbt__cte__LISTS_AB1 as (
+with __dbt__cte__lists_ab1 as (
 
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
--- depends_on: "DB".PUBLIC._AIRBYTE_RAW_LISTS
+-- depends_on: "dev".public._airbyte_raw_lists
 select
-    get_path(parse_json(_airbyte_data), '"golf"') as GOLF,
-    get_path(parse_json(_airbyte_data), '"cricket"') as CRICKET,
-    get_path(parse_json(_airbyte_data), '"football"') as FOOTBALL,
-    _AIRBYTE_AB_ID,
-    _AIRBYTE_EMITTED_AT,
-    current_timestamp as _AIRBYTE_NORMALIZED_AT
-from "dev".PUBLIC._AIRBYTE_RAW_LISTS as table_alias
--- LISTS
+    json_extract_path_text(_airbyte_data, 'golf', true) as golf,
+    json_extract_path_text(_airbyte_data, 'cricket', true) as cricket,
+    json_extract_path_text(_airbyte_data, 'football', true) as football,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    getdate() as _airbyte_normalized_at
+from "dev".public._airbyte_raw_lists as table_alias
+-- lists
 where 1 = 1
 
-),  __dbt__cte__LISTS_AB2 as (
+),  __dbt__cte__lists_ab2 as (
 
 -- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
--- depends_on: __dbt__cte__LISTS_AB1
+-- depends_on: __dbt__cte__lists_ab1
 select
-    GOLF,
-    CRICKET,
-    FOOTBALL,
-    _AIRBYTE_AB_ID,
-    _AIRBYTE_EMITTED_AT,
-    current_timestamp as _AIRBYTE_NORMALIZED_AT
-
-from __dbt__cte__LISTS_AB1
--- LISTS
+    golf,
+    cricket,
+    football,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    getdate() as _airbyte_normalized_at
+from __dbt__cte__lists_ab1
+-- lists
 where 1 = 1
 
-),  __dbt__cte__LISTS_AB3 as (
+),  __dbt__cte__lists_ab3 as (
 
 -- SQL model to build a hash column based on the values of this record
--- depends_on: __dbt__cte__LISTS_AB2
+-- depends_on: __dbt__cte__lists_ab2
 select
-    md5(cast(coalesce(cast(GOLF as
-    varchar
-), '') || '-' || coalesce(cast(CRICKET as
-    varchar
-), '') || '-' || coalesce(cast(FOOTBALL as
-    varchar
-), '') as
-    varchar
-)) as _AIRBYTE_LISTS_HASHID,
+    md5(cast(coalesce(cast(golf as varchar), '') || '-' || coalesce(cast(cricket as varchar), '') || '-' || coalesce(cast(football as varchar), '') as varchar)) as _airbyte_lists_hashid,
     tmp.*
-from __dbt__cte__LISTS_AB2 tmp
--- LISTS
+from __dbt__cte__lists_ab2 tmp
+-- lists
 where 1 = 1
 
 )-- Final base SQL model
--- depends_on: __dbt__cte__LISTS_AB3
+-- depends_on: __dbt__cte__lists_ab3
 select
-    GOLF,
-    CRICKET,
-    FOOTBALL,
-    _AIRBYTE_AB_ID,
-    _AIRBYTE_EMITTED_AT,
-        current_timestamp as _AIRBYTE_NORMALIZED_AT,
-    _AIRBYTE_LISTS_HASHID
-from __dbt__cte__LISTS_AB3
--- LISTS from "DB".PUBLIC._AIRBYTE_RAW_LISTS
+    golf,
+    cricket,
+    football,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    getdate() as _airbyte_normalized_at,
+    _airbyte_lists_hashid
+from __dbt__cte__lists_ab3
+-- lists from "dev".public._airbyte_raw_lists
 where 1 = 1
 
-            ) order by (_AIRBYTE_EMITTED_AT)
- 
+  );
